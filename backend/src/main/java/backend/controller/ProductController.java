8214e404c3b908a1d9c00231e3b25c47f63aa6ff
jackhoppecs <jackhoppecs@gmail.com>
2026-04-27 20:54:56 -0500

    Created a service layer for products and altered controller to call service functions instead of repository directly

--- backend/src/main/java/backend/controller/ProductController.java
+++ backend/src/main/java/backend/controller/ProductController.java
@@ -1,7 +1,7 @@
 package backend.controller;
 
 import backend.model.Product;
-import backend.repository.ProductRepository;
+import backend.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,48 +12,40 @@ import java.util.List;
 // So this controller handles: /api/products
 @RequestMapping("/api/products")
 public class ProductController {
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository){
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService){
+        this.productService = productService;
     }
 
     // This handles GET /api/products
     @GetMapping
     public List<Product> getAllProducts(){
-        return productRepository.findAll();
+        return productService.getAllProducts();
     }
-
-    
 
     // This handles GET /api/products/1 etc
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id){
-        return productRepository.findById(id).orElse(null);
+        return productService.getProductById(id);
     }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct){
         // Find product by Id, if it exists update attributes of the product with updatedProduct data
-        return productRepository.findById(id)
-        .map(product ->{
-            product.setName(updatedProduct.getName());
-            product.setBrand(updatedProduct.getBrand());
-            product.setDescription(updatedProduct.getDescription());
-            return productRepository.save(product);
-        }).orElse(null);
-        // We ant to return the updated version of the resource so we can confirm what was changed and we can avoid using another GET request
+        return productService.updateProduct(id, updatedProduct);
+        // Want to return the updated version of the resource so we can confirm what was changed and we can avoid using another GET request
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id){
         // We don't need a return because we aren't using the data anymore it has been deleted
-        productRepository.deleteById(id);
+        productService.deleteProduct(id);
     }
 
     @PostMapping
     // Spring takes JSON from request body and turns it into a Product object to be saved to DB
     public Product createProduct(@RequestBody Product product){
-        return productRepository.save(product);
+        return productService.createProduct(product);
     }
 }
