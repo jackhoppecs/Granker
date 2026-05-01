d9f00018d5a12bc7ba8c2fc8225c90ea3a811fb2
jackhoppecs <jackhoppecs@gmail.com>
2026-04-30 21:36:14 -0500

    Created controller, repository, and service files for users with basic CRUD operations

---
+++ backend/src/main/java/backend/controller/UserController.java
@@ -0,0 +1,42 @@
+package backend.controller;
+
+import backend.model.User;
+import backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @PostMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser){
+        return userService.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+}
