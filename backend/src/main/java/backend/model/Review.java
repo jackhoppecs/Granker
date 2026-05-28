d96c29fd109baebdcae7b7096bcb834fe3f8957a
jackhoppecs <jackhoppecs@gmail.com>
2026-05-27 20:34:21 -0500

    Added validation for reviews

--- backend/src/main/java/backend/model/Review.java
+++ backend/src/main/java/backend/model/Review.java
@@ -1,6 +1,10 @@
 package backend.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "reviews")
@@ -10,20 +14,32 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @Min(1)
+    @Max(5)
     private int rating;
 
+    // @NotBlank -> Only works on strings
+    @Column(nullable = false)
+    @NotBlank
     private String text;
 
     // A review belongs to one product but a product can have many reviews
     @ManyToOne
     // Join Column tells DB what foreign key colum to create in reviews table
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
+    // @NotNull
     private Product product;
 
     // A review belongs to one user but a user can have many reviews
     @ManyToOne
     // Join Column tells DB what foreign key colum to create in reviews table
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
+    // @NotNull -> About @Valid: with our current request body, this may cause a problem.
+    // {
+    // "rating": 5,
+    // "text": "Good"
+    // }
     private User user;
 
     public Review(){
