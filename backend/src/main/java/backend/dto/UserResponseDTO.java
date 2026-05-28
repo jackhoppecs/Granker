3067317310eb95f59ae10ac62c3b4be5dbf41c89
jackhoppecs <jackhoppecs@gmail.com>
2026-05-28 17:40:01 -0500

    Added CreateReview dto and implemented in Review controller

--- backend/src/main/java/backend/dto/UserResponseDTO.java
+++ backend/src/main/java/backend/dto/UserResponseDTO.java
@@ -1,12 +1,12 @@
 package backend.dto;
 
-public class UserResponseDto{
+public class UserResponseDTO{
 
     private Long id;
     private String username;
     private String email;
 
-    public UserResponseDto(Long id, String username, String email){
+    public UserResponseDTO(Long id, String username, String email){
         this.id = id;
         this.username = username;
         this.email = email;
