282e4e1571ebd4e507e86332bc631a7b8b98e2a2
jackhoppecs <jackhoppecs@gmail.com>
2026-06-03 18:46:32 -0500

    Added react routing for pages and links for product cards to retrieve data for a specific product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 function App() {
-  return <ProductsPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductsPage />}></Route>
+        <Route path="/products" element={<ProductsPage />}></Route>
+        <Route path="/products/:id" element={<ProductDetailsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
