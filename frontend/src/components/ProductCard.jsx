5628d850d1e64de2c1d2a934af31fc8d35a2ab44
jackhoppecs <jackhoppecs@gmail.com>
2026-06-03 18:46:32 -0500

    Added react routing for pages and links for product cards to retrieve data for a specific product

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
+
+// Components are usually a smaller reusable UI piece.
+// So we reuse this ProductCard in our Products page
 function ProductCard({ product }) {
   return (
     <article className="card">
       <h2>{product.name}</h2>
       <p className="brand">{product.brand}</p>
       <p>{product.description}</p>
+
+      <Link to={`/products/${product.id}`}>View Details</Link>
     </article>
   );
 }
