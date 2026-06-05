e6863bad247b1f84c677b6bf615f71796ab6f071
jackhoppecs <jackhoppecs@gmail.com>
2026-06-05 11:17:16 -0500

    Added username field for ReviewCard

--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -1,6 +1,7 @@
 function ReviewCard({ review }) {
   return (
     <article className="card">
+      <p>Name: {review.username} </p>
       <p className="rating">Rating: {review.rating}/5</p>
       <p>{review.text}</p>
     </article>
