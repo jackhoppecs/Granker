031608415577dcdc3fcd04cd9817d96c292e3dfb
jackhoppecs <jackhoppecs@gmail.com>
2026-06-06 12:45:30 -0500

    Updated review card and review card style

--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -1,9 +1,12 @@
 function ReviewCard({ review }) {
   return (
-    <article className="card">
-      <p>Name: {review.username} </p>
-      <p className="rating">Rating: {review.rating}/5</p>
-      <p>{review.text}</p>
+    <article className="review-card">
+      <div className="review-card-header">
+        <p className="review-user">{review.username || "Anonymous"}</p>
+        <p className="review-rating">{review.rating}/5</p>
+      </div>
+
+      <p className="review-text">{review.text}</p>
     </article>
   );
 }
