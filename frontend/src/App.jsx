40be0d8c5c66fc6c15bf5ef003cd68b3314a6b03
jackhoppecs <jackhoppecs@gmail.com>
2026-06-08 20:29:17 -0500

    Added extra route to show create review page in app.jsx and to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CreateProductPage from "./pages/CreateProductPage";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -15,8 +16,11 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
+        {/* These are just paths to where we have these pages not same as APIs */}
         <Route path="/" element={<ProductsPage />}></Route>
         <Route path="/products" element={<ProductsPage />}></Route>
+        {/* This needs to go before Product details route so it doesnt think 'new' is a product id */}
+        <Route path="/products/new" element={<CreateProductPage />}></Route>
         <Route path="/products/:id" element={<ProductDetailsPage />}></Route>
       </Routes>
     </BrowserRouter>
