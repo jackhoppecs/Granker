dcf34c196ce75ee5ceecde639c78490824643ca1
jackhoppecs <jackhoppecs@gmail.com>
2026-06-11 14:05:57 -0500

    Created new prop in App to pass to Navbar to handle logout and re-renders through prop updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+// Name of props passed in MUST MATCH. This is because props are objects being passed in React
+function Navbar({ currentUser, handleLogout }) {
   return (
     <nav className="navbar">
       <Link className="navbar-brand" to="/">
@@ -10,7 +11,15 @@ function Navbar() {
       <div className="navbar-links">
         <Link to="/">Products</Link>
         <Link to="/products/new">Add Product</Link>
-        <Link to="/login">Login</Link>
+        {/* State changes cause components to re render so when currentuser changes from logout this conditional statement
+        under app.jsx reruns since navbar is inside app.jsx. It's like a nested strucutre */}
+        {currentUser ? (
+          <button className="navbar-button" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </div>
     </nav>
   );
