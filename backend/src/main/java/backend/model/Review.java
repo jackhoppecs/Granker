d5e1f19323176f1016d8bad6b1ca0f0e60ee163c
jackhoppecs <jackhoppecs@gmail.com>
2026-06-12 12:32:11 -0500

    Created new fields for time created at and updated at for reviews, implemented in both entity and dto

--- backend/src/main/java/backend/model/Review.java
+++ backend/src/main/java/backend/model/Review.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "reviews")
@@ -41,10 +42,29 @@ public class Review {
     // "text": "Good"
     // }
     private User user;
+    
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Review(){
 
     }
+
+    // When review is created both createdAt and updatedAt set
+    // Run this right before this entity is inserted into DB for the first time
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // When review is edited ONLY updated at changes
+    // Run this method right before this existing entity is updated in DB
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
 
     public Review(int rating, String text){
         this.rating = rating;
