b453ee83b9aeebb6acb4a3ff23e9daa7a5f7ae25
jackhoppecs <jackhoppecs@gmail.com>
2026-07-29 12:09:36 -0500

    Implemented apiFetch error handling for reviews api calls

--- frontend/src/api/reviews.js
+++ frontend/src/api/reviews.js
@@ -1,5 +1,5 @@
 import API_BASE_URL from "./config";
-import { handleApiResponse } from "./apiUtils";
+import { apiFetch } from "./apiUtils";
 
 export async function getReviewsByProductId(productId, sort = "newest") {
   const params = new URLSearchParams();
@@ -7,51 +7,31 @@ export async function getReviewsByProductId(productId, sort = "newest") {
   if (sort) {
     params.append("sort", sort);
   }
-  const response = await fetch(
+
+  return apiFetch(
     `${API_BASE_URL}/api/products/${productId}/reviews?${params.toString()}`,
   );
-
-  if (!response.ok) {
-    return handleApiResponse(response);
-  }
-
-  return response.json();
 }
 
 export async function getMyReviews() {
-  const response = await fetch(`${API_BASE_URL}/api/reviews/me`, {
+  return apiFetch(`${API_BASE_URL}/api/reviews/me`, {
     credentials: "include",
   });
-
-  if (!response.ok) {
-    return handleApiResponse(response);
-  }
-
-  return response.json();
 }
 
 export async function createReview(productId, review) {
-  const response = await fetch(
-    `${API_BASE_URL}/api/products/${productId}/reviews`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(review),
+  return apiFetch(`${API_BASE_URL}/api/products/${productId}/reviews`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
-
-  if (!response.ok) {
-    return handleApiResponse(response);
-  }
-
-  return response.json();
+    credentials: "include",
+    body: JSON.stringify(review),
+  });
 }
 
 export async function updateReview(reviewId, updatedReviewData) {
-  const response = await fetch(`${API_BASE_URL}/api/reviews/${reviewId}`, {
+  return apiFetch(`${API_BASE_URL}/api/reviews/${reviewId}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -59,24 +39,11 @@ export async function updateReview(reviewId, updatedReviewData) {
     credentials: "include",
     body: JSON.stringify(updatedReviewData),
   });
-
-  if (!response.ok) {
-    return handleApiResponse(response);
-  }
-
-  return response.json();
 }
 
 export async function deleteReview(reviewId) {
-  const response = await fetch(`${API_BASE_URL}/api/reviews/${reviewId}`, {
+  return apiFetch(`${API_BASE_URL}/api/reviews/${reviewId}`, {
     method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
     credentials: "include",
   });
-
-  if (!response.ok) {
-    return handleApiResponse(response);
-  }
 }
